17f1ea72f9cb6eda640282a64d64a71ac86717b8
Mike <nhj25252@gmail.com>
2023-08-10 20:43:07 +0900

    routing html files

--- 02-Clients&servers/app.js
+++ 02-Clients&servers/app.js
@@ -8,5 +8,12 @@ app.listen(3000); // returns an instance of a server
 
 // root of the domain
 app.get("/", (req, res) => {
-  res.send("<p>home page</p>"); // instead of res.write..res.end..
+  //res.send("<p>home page</p>"); // instead of res.write..res.end..
+  res.sendFile("./views/index.html", { root: __dirname }); // the root should be 02-Clients&servers
+});
+
+app.get("/about", (req, res) => {
+  // not an ideal way! we want to send a whole html file
+  //res.send("<p>about page</p>");
+  res.sendFile("./views/about.html", { root: __dirname }); // the root should be 02-Clients&servers
 });
