482afd84895dc9c7f1397064053bc4d62af78fd5
unknown <nhj25252@gmail.com>
2023-08-10 16:49:49 +0900

    basic routing

--- server.js
+++ server.js
@@ -10,8 +10,21 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader("Content-Type", "text/html");
 
+  let path = "./views/"; // html files are inside html folder , always begine with ./views
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+  }
+
   // send an html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
     } else {
