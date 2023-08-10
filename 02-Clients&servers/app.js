2a6bc9f1538702aa721a200216d1c4d4c388f36d
Mike <nhj25252@gmail.com>
2023-08-10 20:52:45 +0900

    redirecting and 404 page in express

--- 02-Clients&servers/app.js
+++ 02-Clients&servers/app.js
@@ -17,3 +17,19 @@ app.get("/about", (req, res) => {
   //res.send("<p>about page</p>");
   res.sendFile("./views/about.html", { root: __dirname }); // the root should be 02-Clients&servers
 });
+
+// redirecting with express
+app.get("/about-us", (req, res) => {
+  res.redirect("/about"); // redirect to /about page!
+});
+
+// 404 page
+// to create middleware and fire middleware functions
+// use this function for every single request regardless of the url
+// use() is gonna for every request coming in, but only if..
+// run the entire file, if no matches => carry on, get to the bottom => fire this callback
+
+// MUST GO AT THE BOTTOM!
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
