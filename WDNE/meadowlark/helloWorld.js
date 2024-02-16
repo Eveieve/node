b4a2558081b1e37716644c736cd1995ab94c4e28
unknown <nhj25252@gmail.com>
2024-02-16 23:46:48 +0900

    createServer code

---
+++ WDNE/meadowlark/helloWorld.js
@@ -0,0 +1,43 @@
+const http = require("http");
+
+const fs = require("fs");
+
+function serveStaticFiles(res, path, contentType, responseCode) {
+  if (!responseCode) responseCode = 200;
+
+  fs.readFile(__dirname + path, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("500 - Internal Error");
+    } else {
+      res.writeHead(responseCode, {
+        "Content-Type": contentType,
+      });
+      res.end(data);
+    }
+  });
+}
+
+http
+  .createServer((req, res) => {
+    // normalize url by removing querystring,
+    // optional trailing slash, and making it lowercase
+    const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch (path) {
+      case "":
+        res.writeHead(200, { "Content-type": "text.plain" });
+        res.end("Homepage");
+        break;
+      case "/about":
+        res.writeHead(200, { "Content-type": "text/plain" });
+        res.end("About");
+        break;
+      default:
+        res.writeHead(404, { "Content-type": "text/plain" });
+        res.end("Not found");
+        break;
+    }
+  })
+  .listen(3000);
+
+console.log("server started on localhost:3000; press cntrl-c to terminate");
